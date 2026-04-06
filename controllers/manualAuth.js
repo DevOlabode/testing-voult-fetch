87935a28f5b8de222ec25ba1cb17b3f1c35e7fe6
Samuel Olabode <solabode499@gmail.com>
2026-04-06 16:01:16 -0600

    refactor logout method in manualAuth controller: remove commented-out code and clarify token retrieval process for improved readability

--- controllers/manualAuth.js
+++ controllers/manualAuth.js
@@ -67,45 +67,9 @@ module.exports.register = async (req, res) => {
       });
     }
   };
-  
-
-// module.exports.logout = async(req, res)=>{
-//     try {
-//       const accessToken = await tokenManager.getValidAccessToken();
-//       const response = await axios.post(
-//         `${process.env.API_URL}/auth/logout`,
-//         {},
-//         {
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'x-client-id': `${process.env.CLIENT_ID}`,
-//             'x-client-secret': `${process.env.CLIENT_SECRET}`,
-//             'X-Client-Token': `Bearer ${accessToken}`
-//           }
-//         }
-//       );
-  
-//       console.log(response.data);
-  
-//       res.json({
-//         success: true,
-//         data: response.data
-//       });
-  
-//     } catch(error){
-//       console.error(error.response.data);
-  
-//       res.status(error.response?.status || 500).json({
-//         success: false,
-//         message: error.response?.data || "Something went wrong"
-//       });
-//     }
-//   };
 
 module.exports.logout = async(req, res)=>{
   try {
-    // Use getCurrentToken() instead of getValidAccessToken() for logout
-    // This prevents auto-refreshing the token when logging out
     const currentToken = tokenManager.getCurrentToken();
     
     if (!currentToken) {
